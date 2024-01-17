cc38301ea78b77ba3c89d2e3a548ade446105e0f
kiboma <omayioben@gmail.com>
2024-01-17 18:58:49 +0300

    Add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
-//import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import LogoImg from '../assets/logo.jpeg'
+
+import '../App.css'
 
 export const Header = () => {
   return (
     <header className='flex justify-around flex-wrap items-center'>
-        <div className='flex justify-around flex-wrap items-center'>
+        <Link to="/" className='flex justify-around flex-wrap items-center'>
             <img src={LogoImg} alt="Logo" />
-            <p className='text-2xl' >Shopping Cart</p>
-        </div>
+            <p className='text-3xl' >Shopping Cart</p>
+        </Link>
         <div className='text-2xl '>
-            <span className='mx-2'>Home</span>
-            <span className='mx-2'>Cart</span>
+            <NavLink to="/" className='mx-2' end>Home</NavLink>
+            <NavLink to="/cart" className='mx-2'>Cart</NavLink>
         </div>
         <div className='text-2xl'>
-            <span>Cart:2</span>
+            <NavLink to="/cart">Cart:2</NavLink>
         </div>
 
     </header>
