2c0b59d8478993e83a257933fadad9155b96a826
kiboma <omayioben@gmail.com>
2024-01-26 08:35:44 +0300

    Use list in cart

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -9,152 +9,38 @@ import image5 from '../assets/5.avif';
 
 
 export const Products = ({title}) => {
+
+    const products=[
+        {id: 145, name:"Sony Wh-Ch510 Bluetooth Wireless", price:145, image:image1},
+        {id: 146, name:"boAt Rockerz 450", price:455, image:image2},
+        {id: 147, name:"JBL Tune 760NC", price:145, image:image3},
+        {id: 148, name:"Logitech H111 Wired", price:455, image:image4},
+        {id: 149, name:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport", price:455, image:image5},
+        {id: 150, name:"boAt Rockerz 450", price:455, image:image2},
+
+    ]
+
     useTitle({title})
   return (
     <main className='flex justify-center gap-10 flex-wrap mx-40 '  >
-
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        { products && products.map((product)=>(
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src={image1} alt="product " />
+                <img className="p-8 rounded-t-lg" src={product.image} alt={product.name} />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
-                    <h5 className="text-lg tracking-tight text-gray-900 mb-2 dark:text-white">Sony Wh-Ch510 Bluetooth Wireless</h5>
+                    <h5 className="text-lg tracking-tight text-gray-900 mb-2 dark:text-white">{product.name}</h5>
                 </a>
 
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$299</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">${product.price}</span>
                     <a href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
                 </div>
             </div>
         </div>
 
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src={image2} alt="product " />
-            </a>
-            <div className="px-5 pb-5">
-                <a href="/">
-                    <h5 className="text-lg tracking-tight text-gray-900 mb-2 dark:text-white">boAt Rockerz 450</h5>
-                </a>
-
-                <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$42</span>
-                    <a href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
-                </div>
-            </div>
-        </div>
-
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src={image3} alt="product " />
-            </a>
-            <div className="px-5 pb-5">
-                <a href="/">
-                    <h5 className="text-lg tracking-tight text-gray-900 mb-2 dark:text-white">JBL Tune 760NC</h5>
-                </a>
-
-                <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$123</span>
-                    <a href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
-                </div>
-            </div>
-        </div>
-
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src={image4} alt="product " />
-            </a>
-            <div className="px-5 pb-5">
-                <a href="/">
-                    <h5 className="text-lg tracking-tight text-gray-900 mb-2 dark:text-white">Logitech H111 Wired</h5>
-                </a>
-
-                <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$365</span>
-                    <a href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
-                </div>
-            </div>
-        </div>
-
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src={image5} alt="product " />
-            </a>
-            <div className="px-5 pb-5">
-                <a href="/">
-                    <h5 className="text-lg tracking-tight text-gray-900 mb-2 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-                </a>
-
-                <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                    <a href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
-                </div>
-            </div>
-        </div>
-
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src={image1} alt="product " />
-            </a>
-            <div className="px-5 pb-5">
-                <a href="/">
-                    <h5 className="text-lg tracking-tight text-gray-900 mb-2 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-                </a>
-
-                <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                    <a href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
-                </div>
-            </div>
-        </div>
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src={image1} alt="product " />
-            </a>
-            <div className="px-5 pb-5">
-                <a href="/">
-                    <h5 className="text-lg tracking-tight text-gray-900 mb-2 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-                </a>
-
-                <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                    <a href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
-                </div>
-            </div>
-        </div>
-
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src={image1} alt="product " />
-            </a>
-            <div className="px-5 pb-5">
-                <a href="/">
-                    <h5 className="text-lg tracking-tight text-gray-900 mb-2 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-                </a>
-
-                <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                    <a href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
-                </div>
-            </div>
-        </div>
-
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src={image1} alt="product " />
-            </a>
-            <div className="px-5 pb-5">
-                <a href="/">
-                    <h5 className="text-lg tracking-tight text-gray-900 mb-2 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-                </a>
-
-                <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                    <a href="/" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
-                </div>
-            </div>
-        </div>      
+        ))}
     </main>
   )
 }
